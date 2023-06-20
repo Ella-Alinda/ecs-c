662e739ba3adccf69b9f25c4d240193d9606bd93
Ella_Alinda <ella@ilolicon.com>
2023-06-20 16:15:43 +0800

    Bug: Change Mem Bench

--- include/membench.hpp
+++ include/membench.hpp
@@ -8,26 +8,28 @@
 // 测试内存读性能并返回结果
 double testMemoryReadPerformance(uint64_t* data, int size) {
     auto startTime = std::chrono::high_resolution_clock::now();
-    volatile long long sum = 0; // 使用 volatile 避免编译器优化
-    for (int i = 0; i < size; ++i) {
+    long long sum = 0;
+    for (int i = 0; i < size; i += 2) {
         sum += data[i];
+        sum += data[i + 1];
     }
     auto endTime = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime);
-    double dataRate = static_cast<double>(size * sizeof(uint64_t)) / (1024 * 1024) / (duration.count() / 1e9);
-    return dataRate;
+    double transferRateMB = (static_cast<double>(size * sizeof(uint64_t)) / 1048576) / (duration.count() / 1e9);
+    return transferRateMB;
 }
 
 // 测试内存写性能并返回结果
 double testMemoryWritePerformance(uint64_t* data, int size) {
     auto startTime = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; i += 2) {
         data[i] = i;
+        data[i + 1] = i + 1;
     }
     auto endTime = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime);
-    double dataRate = static_cast<double>(size * sizeof(uint64_t)) / (1024 * 1024) / (duration.count() / 1e9);
-    return dataRate;
+    double transferRateMB = (static_cast<double>(size * sizeof(uint64_t)) / 1048576) / (duration.count() / 1e9);
+    return transferRateMB;
 }
 
 #endif 
